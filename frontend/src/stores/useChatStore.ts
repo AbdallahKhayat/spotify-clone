1b008d7345729f884230ea877f67ef8a59375169
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-04-22 22:09:44 +0300

    created disconnectSocket function

--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -116,6 +116,12 @@ export const useChatStore = create<ChatStore>((set, get) => ({
       set({ isConnected: true });
     }
   },
-  disconnectSocket: () => {},
+
+  disconnectSocket: () => {
+    if (get().isConnected) {
+      socket.disconnect();
+      set({ isConnected: false });
+    }
+  },
   sendMessage: (receiverId: string, senderId: string, content: string) => {},
 }));
